540f36deea840ad8972d93057aa85ebc3a661609
tongxiinwang <mukjim@gmail.com>
2020-11-30 14:51:31 +0900

    simple http server using net/http package end.

--- serv.go
+++ serv.go
@@ -11,10 +11,8 @@ import (
 const retStr = "<!doctype html>\r\n<html>\r\n  <head>\r\n    <meta charset=\"utf-8\">\r\n    <title></title>\r\n  </head>\r\n  <body>\r\n    <p>it works!</p>\r\n  </body>\r\n</html>\r\n"
 
 func main() {
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	// fmt.Println("Hello world!")
-	fmt.Println("running")
-	fmt.Println(retStr)
 
 	h1 := func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, retStr)
@@ -26,12 +24,6 @@ func main() {
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/endpoint", h2)
 
-	// s := &http.Server{
-	// 	Addr:           ":8080",
-	// 	Handler:        defaultHandler,
-	// 	ReadTimeout:    5 * time.Second,
-	// 	WriteTimeout:   5 * time.Second,
-	// 	MaxHeaderBytes: 1 << 20,
-	// }
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("server is running")
 }
